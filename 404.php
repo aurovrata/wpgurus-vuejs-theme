e2e7323711751ded062b2d7078fd34b9ab91c565
aurovrata <vrata@syllogic.in>
2019-10-21 12:12:21 +0530

    404 fix

--- 404.php
+++ 404.php
@@ -4,27 +4,46 @@
 */
 get_header();
 ?>
-  <!-- VueJs templates -->
-  <template id="body-content">
-    <main>
-      <header>
-        <logo-image v-bind:logo="this.data.logo"></logo-image>
-        <network-menu  v-if="hasMenu('network')" v-bind:menu="this.data.menus.network"></network-menu>
-        <language-menu v-if="hasMenu('languages')" v-bind:current="this.data.lang" v-bind:languages="this.data.menus.languages"></language-menu>
-        <primary-menu  v-if="hasMenu('primary')" v-bind:menu="this.data.menus.primary"></primary-menu>
-      </header>
-      <content-page>
-        <template slot="content">
-            <?php get_template_part( 'templates/index', 'page' );?>
-        </template>
-      </content-page>
+<!-- VueJs templates -->
+<template id="body-content">
+  <main v-bind:class="pageClass()">
+    <header>
+      <logo-image v-bind:logo="this.data.logo"></logo-image>
+
+    <?php if ( has_nav_menu( 'network' ) ) :?>
+      <network-menu v-bind:menu="this.data.menus.network"></network-menu>
+    <?php endif;?>
+
+    <?php if ( has_nav_menu( 'languages' ) ) :?>
+      <language-menu v-if="hasMenu('languages')" v-bind:current="this.data.lang" v-bind:languages="this.data.menus.languages"></language-menu>
+    <?php endif;?>
+
+    <?php if ( has_nav_menu( 'primary' ) ) :?>
+      <primary-menu  v-if="hasMenu('primary')" v-bind:menu="this.data.menus.primary"></primary-menu>
+    <?php endif;?>
+
+    </header>
+      <section class="page-content page-404">
+          <component v-bind:is="currentComponent" v-bind:rootdata="this.data">
+            <template slot="content">
+              <div class="single-page">
+                <?php get_template_part( 'templates/index', 'page' );?>
+              </div>
+            </template>
+          </component>
+      </section>
       <footer>
+      <?php if ( has_nav_menu( 'footer' ) ) :?>
         <footer-menu  v-if="hasMenu('footer')" v-bind:menu="this.data.menus.footer"></footer-menu>
+      <?php endif;?>
       </footer>
+
     </main>
   </template>
-  <!-- VueJs templates -->
-  <template id="content-page">
+  <!-- VueJs templates: the static-template is using slots
+  and therefore the page content templates have direct access
+  to the parent component data, ie this.data.posts -->
+  <template id="static-template">
     <div class="vue-content">
       <slot name="content"></slot>
     </div>
@@ -32,8 +51,9 @@ get_header();
   <?php
   /*load templates for vue components*/
   if(apply_filters('wpgurus_theme_multilingual', false) || defined ("POLYLANG_VERSION")){
-    get_template_part('templates/index', 'languages');
+    include_vue_template('main', 'languages');
   }
+
   if ( has_nav_menu( 'network' ) ){
     set_query_var('template_id', 'network');
     get_template_part('templates/index', 'menu');
@@ -47,7 +67,8 @@ get_header();
     get_template_part('templates/index', 'menu');
   }
   get_template_part('templates/index', 'logo');
-
+  /** @since 1.2.0 allow plugins to print templates */
+  do_action('wpgurus_theme_print_vuejs_templates');
   ?>
   <!-- end VueJs templates -->
   <?php get_footer();?>
